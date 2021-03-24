e25578ecc521b66cb3e02a35f6db302a461e2184
pranjay-poddar <pranjaypoddar@outlook.com>
2021-03-24 19:13:41 +0530

    added links

--- src/app/globe-track/globe-track.component.ts
+++ src/app/globe-track/globe-track.component.ts
@@ -1,3 +1,4 @@
+import { GlobalDataSummary } from './../models/global-dat';
 import { DataServiceService } from './../services/data-service.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,6 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./globe-track.component.css']
 })
 export class GlobeTrackComponent implements OnInit {
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  // loading = true;
+   globalData: GlobalDataSummary[]=[];
+  // datatable = [];
+  // chart = {
+  //   PieChart : "PieChart" ,
+  //   ColumnChart : 'ColumnChart' ,
+  //   LineChart : "LineChart", 
+  //   height: 500, 
+  //   options: {
+  //     animation:{
+  //       duration: 1000,
+  //       easing: 'out',
+  //     },
+  //     is3D: true
+  //   }  
+  // }
 
   constructor(private DataService : DataServiceService) { }
 
@@ -16,7 +37,23 @@ export class GlobeTrackComponent implements OnInit {
       {
         next : (result)=>{
           console.log(result);
-        }
+          this.globalData = result;
+          result.forEach(cs => {
+            if (!Number.isNaN(cs.confirmed)) {
+              this.totalActive += cs.active
+              this.totalConfirmed += cs.confirmed
+              this.totalDeaths += cs.deaths
+              this.totalRecovered += cs.active
+            }
+
+          })
+
+          // this.initChart('c');
+        }, 
+        // complete : ()=>{
+        //   this.loading = false;
+        
+        // }
       }
     )
   }

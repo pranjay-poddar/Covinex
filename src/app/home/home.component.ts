64b9cbac4f02d8f34d7e125a4d384f7eebed1843
pranjay-poddar <pranjaypoddar@outlook.com>
2021-03-26 08:58:55 +0530

    added link  to btn

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { SignupComponent } from './../signup/signup.component';
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -6,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+    //openDialog--
+    openDialog(){
+      this.dialog.open(SignupComponent);
+    }
 
 }

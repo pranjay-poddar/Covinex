edc2921ac553a543051244758c13ec2ab25fa020
pranjay-poddar <pranjaypoddar@outlook.com>
2021-04-02 01:42:55 +0530

    corrected chat bot

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analysis.component.css']
 })
 export class AnalysisComponent implements OnInit {
-  emailPattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
-  namepattern="[A-Za-z ]{1,32}";
+ 
   constructor() { }
 
   ngOnInit(): void {
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  emailPattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+  namepattern="[A-Za-z ]{1,32}";
   constructor() { }
 
   ngOnInit(): void {

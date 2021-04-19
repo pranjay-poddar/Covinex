394ad9995697897214d17bf4ccfa88c5e0f9773c
pranjay-poddar <pranjaypoddar@outlook.com>
2021-04-20 00:14:22 +0530

    added privicy policy and terms and condition

---
+++ src/app/question-class.ts
@@ -0,0 +1,11 @@
+export class QuestionClass {
+    "id" ?: number;
+    "question": string;
+    "a": string;
+    "b": string;
+    "c": string;
+    "d": string;
+    "answer": string;
+    "selected" ?: string;
+
+}

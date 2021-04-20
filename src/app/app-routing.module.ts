ad53a3caffa02b1a36077fe97ee72804f28f8742
pranjay-poddar <pranjaypoddar@outlook.com>
2021-04-21 02:07:31 +0530

    made some minor corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled', // Add options right here
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/center/center.component.ts
+++ src/app/center/center.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-center',
   templateUrl: './center.component.html',
